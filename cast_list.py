0e137caeb20dd03b86ea82c59b1f3f289bdad6aa
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-10-11 14:37:20 -0500

    :sparkles: Valid names checked with assemble_verified_cast
    imdbpy library added, when encountering new name in cast entry it will be run through search_person() which returns a weighted fuzzy search of all possible results, exact matches listed first. Used to determine whether cast member name is valid

--- cast_list.py
+++ cast_list.py
@@ -1,6 +1,9 @@
 # Load JSON function
 import json
 
+# load imdb library for cast name verification
+from imdb import IMDb, IMDbError
+
 # open movies.json and load it into python list
 input_file = open('data/movies.json')
 movies = json.load(input_file)
@@ -24,4 +27,31 @@ def assemble_cast(movie_list):
 
 
 
-assemble_cast(movies)
+def assemble_verified_cast(movie_list):
+    cast_dict = {}
+    invalid_dict = {}
+
+    for movie in movie_list:
+        for member in movie["cast"]:
+            key=f'{member},{movie["year"]}'
+            if key in cast_dict:
+                cast_dict[key] += 1
+            elif key not in invalid_dict:
+                try:
+                    ia=IMDb()
+                    people = ia.search_person(member)
+                    if len(people) >0 and people[0]['name']==member:
+                        cast_dict[key]=1
+                    else:
+                        invalid_dict[key]=1
+                except IMDbError as err:
+                    print(err)
+
+    for key in sorted(cast_dict.keys()):
+        print(f'{key},{cast_dict[key]}')
+
+    # unknown length of full output due to exceptional runtime
+
+
+assemble_verified_cast(movies[:250])
+# assemble_cast(movies)
