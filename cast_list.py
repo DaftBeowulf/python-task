a6df92bbb94d11d2c86de8197d6bdf9705fd18f9
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-10-11 15:01:29 -0500

    :speech_balloon: Doc strings, clarifying comments added

--- cast_list.py
+++ cast_list.py
@@ -12,7 +12,18 @@ input_file = open('data/movies.json')
 movies = json.load(input_file)
 
 
+
 def assemble_cast(movie_list):
+    """
+    Accepts list of movies. For each comma-separated cast member + movie year combo, stores
+    entry into lookup table cast_dict for instant subsequent lookup times.
+
+    node example: {
+        'Zoey Deschanel,2011' : 2
+    }
+
+    Sorts keys alphabetically and prints each line as <name>,<year>,<# of films>
+    """
     cast_dict = {}
 
     for movie in movie_list:
@@ -28,7 +39,23 @@ def assemble_cast(movie_list):
     # length of output: 55484
 
 
+
 def assemble_verified_cast(movie_list):
+    """
+    Accepts list of movies. For new cast member + movie year combo encountered,
+    uses the imdbpy library to verify if cast member exists in imdb database.
+    For each comma-separated cast member + movie year combo, stores
+    entry into lookup table cast_dict for instant subsequent lookup times.
+
+    node example: {
+        'Zoey Deschanel,2011' : 2
+    }
+
+    Sorts keys alphabetically and prints each line as <name>,<year>,<# of films>
+
+    This method provides truest verification, but not ideal due to time of each API call
+    to imdb's database for each search_person call.
+    """
     cast_dict = {}
     invalid_dict = {}
 
@@ -38,6 +65,10 @@ def assemble_verified_cast(movie_list):
             if key in cast_dict:
                 cast_dict[key] += 1
             elif key not in invalid_dict:
+                # Imdb's search_person() method uses weighted fuzzy search for possible name matches,
+                # with exact match (if found) listed first. If search_person returns nothing or the first 
+                # match is not exact, name is either not valid actor/actress or not valid name, and stored
+                # in invalid_dict for faster subsequent lookup times.
                 try:
                     ia=IMDb()
                     people = ia.search_person(member)
@@ -55,6 +86,24 @@ def assemble_verified_cast(movie_list):
 
 
 def assemble_pseduo_verified_cast(movie_list):
+    """
+    Accepts list of movies. For new cast member + movie year combo encountered,
+    performs loose validation to see if name is capitalized or if name is 'will.i.am'
+    as his is currently the only non-capitalized actor's name.
+    For each comma-separated cast member + movie year combo, stores
+    entry into lookup table cast_dict for instant subsequent lookup times.
+
+    node example: {
+        'Zoey Deschanel,2011' : 2
+    }
+
+    Sorts keys alphabetically and prints each line as <name>,<year>,<# of films>
+
+    Currently eliminate ~500 invalid entries, but this method provides only loose validation 
+    and not reliable in every case:
+    -Does not catch capitalized non-name string instead of a valid name
+    -Would not catch new actor with an all-lowercase name    
+    """
     cast_dict = {}
 
     for movie in movie_list:
